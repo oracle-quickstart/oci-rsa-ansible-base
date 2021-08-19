ea96eeb642272f2fbb58dde465b7701179e6c19f
Yupei Yang <yupei.yang@oracle.com>
2021-08-19 10:47:44 -0700

    CAAS-1020: Refactor Software Inventory

--- templates/software_inventory.sh
+++ templates/software_inventory.sh
@@ -1,2 +1,5 @@
+id=$(curl -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/id)
+file_name=software-inventory-$id-"`date +"%d-%m-%Y"`".csv
 rpm -qa --queryformat '%{NAME}, "%{VENDOR}", %{VERSION}, "%{LICENSE}", "%{SUMMARY}"\n' | sort > {{ temp_report_path }}/inventory.csv
-oci os object put -bn {{ resource_bucket_name }} --name {{ file_name }} --file {{ temp_report_path }}/inventory.csv --force --auth instance_principal
+oci os object put -bn {{ resource_bucket_name }} --name $file_name --file {{ temp_report_path }}/inventory.csv --force --auth instance_principal
+rm {{ temp_report_path }}/inventory.csv
